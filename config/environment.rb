ddce3d6256d19c09429aa3203f36567bef32a501
Brian Franklin <brianfranklinmusic@gmail.com>
2020-12-30 17:42:16 -0500

    first push

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,22 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+configure :development do
+  set :database, "sqlite3://database.db"
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host => db.host,
+    :username => db.username,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
 
 MusicBrainz.configure do |c|
   c.app_name = "RecordCollection"
@@ -16,3 +28,16 @@ end
 
 require './app/controllers/application_controller'
 require_all 'app'
+
+# configure :production do 
+#   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+#   ActiveRecord::Base.establish_connection(
+#     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#     :host => db.host,
+#     :username => db.user,
+#     :password => db.password,
+#     :database => db.path[1..-1],
+#     :encoding => 'utf8'
+#   )
+# end
--- db/migrate/20201230223846_create_albums.rb
+++ db/migrate/20201230223846_create_albums.rb
@@ -2,8 +2,8 @@ class CreateAlbums < ActiveRecord::Migration
   def change
     create_table :albums do |t|
       t.string :name
-      t.integer :artist_id
       t.integer :user_id
+      t.integer :artist_id
       t.integer :year_released
       t.string :genre
       t.string :notes
