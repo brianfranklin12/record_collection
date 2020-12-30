a0fb0c80f549825c7648b173b881d37a9a47334c
Brian Franklin <brianfranklinmusic@gmail.com>
2020-12-30 18:22:31 -0500

    Revert "huge update, redid tables, added imgs to index etc"
    This reverts commit 7563f6245acae08aaed503d96ef369e3b49deb76.

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 configure :development do
-  set :database, "sqlite3://database.db"
+  ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
 end
 
 configure :production do
